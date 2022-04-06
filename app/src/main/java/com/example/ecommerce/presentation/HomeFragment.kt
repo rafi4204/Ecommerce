7685c6c438b9c96167ee7bbeed7307c37e553f1e
rafi <rafiwilder4204@gmail.com>
2022-04-06 21:03:56 +0600

    add dependency, navigation graph

---
+++ app/src/main/java/com/example/ecommerce/presentation/HomeFragment.kt
@@ -0,0 +1,28 @@
+package com.example.ecommerce.presentation
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.ecommerce.R
+
+class HomeFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = HomeFragment()
+    }
+
+    private lateinit var viewModel: HomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.home_fragment, container, false)
+    }
+
+
+
+}
